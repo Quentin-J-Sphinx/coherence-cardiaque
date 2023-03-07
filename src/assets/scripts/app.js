4a63096637e200f85e602353e154b5d1d272e98c
Quentin-J-Sphinx <quentin.janowszik@gmail.com>
2023-03-07 18:32:59 +0100

    Mobile responsive

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,31 +1,4 @@
-const startBtn = document.getElementById('start');
-const sctElt = document.createElement('section');
-const divElt = document.createElement('div');
-const timerElt = document.createElement('p');
-const countdownElt = document.createElement('span');
-// countdownElt.textContent = "5:00";
-countdownElt.setAttribute('id', 'countdown');
-
-const inputElt = document.getElementById('duration');
-const barElt = document.getElementById('bar')
-
-let timeSet = 5;
-
-inputElt.addEventListener('input', () => {
-    timeSet = parseFloat(inputElt.value);
-    barElt.value = parseFloat(inputElt.value);
-})
-
-barElt.addEventListener('input', () => {
-    timeSet = parseFloat(barElt.value);
-    inputElt.value = parseFloat(barElt.value);
-})
-
-// inputElt.addEventListener('input', () => {
-    // barElt.value = inputElt.value;
-// })
-
-startBtn.addEventListener('click', (e) => {
+function start(e) {
     e.preventDefault();
 
     sctElt.classList.add('divBreathe');
@@ -34,36 +7,61 @@ startBtn.addEventListener('click', (e) => {
     
     const respirationElt = document.getElementById('breathe-anim');
     const osciElt = document.getElementById('osc');
-
+    
     if (respirationElt.checked === true){
-    divElt.classList.add('divBreathe-round');
-    divElt.classList.remove('divBreathe-osc');
+        divElt.classList.add('divBreathe-round');
+        divElt.classList.remove('divBreathe-osc');
     }
     
     else if (osciElt.checked === true){
         divElt.classList.add('divBreathe-osc');
         divElt.classList.remove('divBreathe-round');
     }
-    // else if (respirationElt.checked === false){
-    //     divElt.classList.remove('divBreathe-round');
-    // }
-    // else if (osciElt.checked === false){
-    //     divElt.classList.remove('divBreathe-osc');
-    // }
+    
+    const valueSelect = selectElt.value;
+    if (divElt.classList.contains('divBreathe-round')){
+        if (valueSelect === '5'){
+        divElt.className ='divBreathe-round';
+        }
+        else if(valueSelect === '6'){
+            divElt.className = 'divBreathe-round divBreathe-round--six';
+        }
+        else if (valueSelect === '4'){
+            divElt.className = 'divBreathe-round divBreathe-round--quatre';
+        }
+        else if (valueSelect === '3'){
+            divElt.className = 'divBreathe-round divBreathe-round--trois';
+        }
+    }
+
+    else if(divElt.classList.contains('divBreathe-osc')){
+        if (valueSelect === '5'){
+            divElt.className ='divBreathe-osc';
+            }
+            else if(valueSelect === '6'){
+                divElt.className = 'divBreathe-osc divBreathe-osc--six';
+            }
+            else if (valueSelect === '4'){
+                divElt.className = 'divBreathe-osc divBreathe-osc--quatre';
+            }
+            else if (valueSelect === '3'){
+                divElt.className = 'divBreathe-osc divBreathe-osc--trois';
+            }
+    }
+        console.log(valueSelect);
 
     sctElt.append(divElt);
-
     divElt.append(timerElt);
     timerElt.append(countdownElt);
-
+    
+    
     const checkElt = document.getElementById('sound');
     const audioElt = document.getElementById('audio-player');
     
     if (checkElt.checked === true){
         audioElt.play();
     }
-
-
+    
 
     // Convert minutes to seconds
     let time = timeSet* 60;
@@ -105,4 +103,36 @@ sctElt.addEventListener('click', () => {
         audio.pause();
     }
 })
+}
+
+const startBtn = document.getElementById('start');
+const startMobile = document.getElementById('start-mobile')
+const sctElt = document.createElement('section');
+const divElt = document.createElement('div');
+const timerElt = document.createElement('p');
+const selectElt = document.getElementById('breathe');
+const countdownElt = document.createElement('span');
+// countdownElt.textContent = "5:00";
+countdownElt.setAttribute('id', 'countdown');
+
+const inputElt = document.getElementById('duration');
+const barElt = document.getElementById('bar')
+
+let timeSet = 5;
+
+inputElt.addEventListener('input', () => {
+    timeSet = parseFloat(inputElt.value);
+    barElt.value = parseFloat(inputElt.value);
 })
+
+barElt.addEventListener('input', () => {
+    timeSet = parseFloat(barElt.value);
+    inputElt.value = parseFloat(barElt.value);
+})
+
+// inputElt.addEventListener('input', () => {
+// barElt.value = inputElt.value;
+// })
+
+startBtn.addEventListener('click', start);
+startMobile.addEventListener('click', start);
